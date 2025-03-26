dfd03779f075f8c28bfdf4f1c0e46f878bfc069d
Viviana <vi.gmorales@gmail.com>
2025-03-25 19:52:42 -0500

    Added custom filter

--- module7-solution/js/app.js
+++ module7-solution/js/app.js
@@ -4,7 +4,8 @@
     angular.module('ShoppingListCheckOff', [])
         .controller('ToBuyController', ToBuyController)
         .controller('AlreadyBoughtController', AlreadyBoughtController)
-        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+        .filter('money', MoneyFilter);
 
     ToBuyController.$inject = ['ShoppingListCheckOffService'];
     function ToBuyController(ShoppingListCheckOffService) {
@@ -17,21 +18,26 @@
         };
     }
 
-    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
-    function AlreadyBoughtController(ShoppingListCheckOffService) {
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService', 'moneyFilter'];
+    function AlreadyBoughtController(ShoppingListCheckOffService, moneyFilter) {
         var bought = this;
+
         bought.items = ShoppingListCheckOffService.getBought();
+
+        bought.message = function (itemIndex) {
+            ShoppingListCheckOffService.boughtMessage(itemIndex);
+        }
     }
 
     function ShoppingListCheckOffService() {
         var service = this;
 
         // List of shopping items
-        var to_buy_items = [{ name: "cookies", quantity: 10, pricePerItem: 3.95 },
+        var to_buy_items = [{ name: "cookies", quantity: 10, pricePerItem: 4 },
         { name: "popcorn", quantity: 5, pricePerItem: 1 },
-        { name: "grapes", quantity: 12, pricePerItem: 3.25 },
-        { name: "yogurts", quantity: 8, pricePerItem: 2.50 },
-        { name: "potatoes", quantity: 15, pricePerItem: .25 }
+        { name: "grapes", quantity: 12, pricePerItem: 3 },
+        { name: "yogurts", quantity: 8, pricePerItem: 2 },
+        { name: "potatoes", quantity: 16, pricePerItem: .25 }
         ];
 
         var bought_items = [];
@@ -49,6 +55,13 @@
             to_buy_items.splice(itemIndex, 1);
             bought_items.push(item);
         };
+    }
 
-    };
+    function MoneyFilter() {
+        return function (input) {
+            input = input || "";
+            input = input.replace('$$$', '$');
+            return input;
+        };
+    }
 })();
